edd9b8752070b09bae4a6d01153e410227586e81
Kritika Gupta <guptakritika2001@gmail.com>
2025-11-30 18:32:03 +0530

    ui fixes

--- src/components/home/Faq.jsx
+++ src/components/home/Faq.jsx
@@ -53,10 +53,16 @@ export default function FAQSection() {
   };
 
   return (
-    <div className="pt-20 flex flex-col h-auto">
-
+    <div className="xl:pt-20 lg:pt-20 md:pt-10 pt-10 flex flex-col h-auto">
+      <div
+        className="w-full flex flex-col items-center justify-center text-center px-4 py-10"
+        style={{
+          background:
+            "linear-gradient(180deg, rgba(254, 205, 140, 0.40) 0%, rgba(163, 163, 163, 0.00) 100%)",
+        }}
+      >
       <main className="flex-grow max-w-3xl mx-auto p-6 pt-70">
-        <h2 className="text-3xl font-bold text-center mb-8">
+        <h2 className="xl:text-5xl text-3xl font-semibold text-center text-[#000] mb-8">
           Frequently asked questions
         </h2>
         <div className="space-y-4">
@@ -64,21 +70,21 @@ export default function FAQSection() {
             <div key={index} className="bg-gray-50 rounded-lg shadow-sm">
               <button
                 onClick={() => toggleFAQ(index)}
-                className="w-full flex justify-between items-center p-4 text-left font-medium text-lg focus:outline-none"
+                className="w-full flex justify-between items-center p-4 text-left font-semibold font-montserrat text-[#5D5D5D] text-[20px] focus:outline-none"
               >
                 {faq.question}
-                <span className="text-xl">
+                <span className="text-2xl">
                   {openIndex === index ? "−" : "+"}
                 </span>
               </button>
               {openIndex === index && (
-                <div className="p-4 pt-0 text-gray-600">{faq.answer}</div>
+                <div className="p-4 pt-0 text-[16px] text-[#5D5D5D] text-montserrat">{faq.answer}</div>
               )}
             </div>
           ))}
         </div>
       </main>
-
+      </div>
     </div>
   );
 }
